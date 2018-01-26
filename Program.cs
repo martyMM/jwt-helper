07a575ca60ede52d95d1267d904a92edb4787ae7
Marty McKenna <marty.mckenna@rhinodox.com>
2018-01-26 13:15:53 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace JWTConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var key = new byte[32];
+            RNGCryptoServiceProvider.Create().GetBytes(key);
+            var base64Secret = Convert.ToBase64String(key);
+            var clientId = RandomString(32);
+
+            Console.WriteLine("JWT secret:");
+            Console.WriteLine(base64Secret);
+
+            Console.WriteLine("ClientID");
+            Console.WriteLine(clientId);
+
+            Console.ReadLine();
+        }
+
+        private static string RandomString(int length)
+        {
+            const string pool = "1234567890abcdefghijkmnopqrstuvwxyz0123456789";
+            var builder = new StringBuilder();
+
+            Random rnd = new Random();
+
+            for (var i = 0; i < length; i++)
+            {
+                var c = pool[rnd.Next(0, pool.Length)];
+                builder.Append(c);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
